5ecacc3678e46d761ac75d20d5f8ed1ee8c3a36c
RavusiNbO <ravaravus@vk.com>
2025-03-07 18:23:10 +0300

    all tests

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -123,7 +123,9 @@ class FollowViewSet(
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        if models.Follow.objects.filter(user=user, following=following).exists():
+        if models.Follow.objects.filter(
+            user=user,
+            following=following).exists():
             return Response(
                 {"detail": "You are already following this user."},
                 status=status.HTTP_400_BAD_REQUEST,
@@ -132,3 +134,4 @@ class FollowViewSet(
         follow = models.Follow.objects.create(user=user, following=following)
         serializer = serializers.FollowSerializer(follow)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
